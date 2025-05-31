ffa82e92edded887fe20306d3134a8a682421aa3
Heberto Mayorquin <h.mayorquin@gmail.com>
2025-05-31 17:58:22 -0600

    add interface (#12)

--- src/surmeier_lab_to_nwb/zhai2025/__init__.py
+++ src/surmeier_lab_to_nwb/zhai2025/__init__.py
@@ -1,3 +1,4 @@
+from .bot_interface import PrairieViewBrightnessOverTimeInterface  # noqa: F401
 from .interfaces import (
     PrairieViewIntracellularRecordingInterface,  # noqa: F401
     PrairieViewLineScanInterface,  # noqa: F401
