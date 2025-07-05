3332f6e0ec94ec1395dc51cd88748772620fdf7f
Heberto Mayorquin <h.mayorquin@gmail.com>
2025-07-04 19:18:57 -0600

    Add figure 6 dendritic excitability (#21)

--- src/surmeier_lab_to_nwb/zhai2025/__init__.py
+++ src/surmeier_lab_to_nwb/zhai2025/__init__.py
@@ -1,8 +1,5 @@
 from .bot_interface import PrairieViewBrightnessOverTimeInterface  # noqa: F401
 from .image_stack_interfaces import NikonImageStackInterface  # noqa: F401
-from .interfaces import (
-    PrairieViewIntracellularRecordingInterface,  # noqa: F401
-)
 from .ophys_interfaces import (
     PrairieViewLineScanInterface,  # noqa: F401
 )
