081c22ae7721c73d8868778d7e3b5892188668a9
Heberto Mayorquin <h.mayorquin@gmail.com>
2025-07-23 12:07:21 -0600

    figure 5 re-structure

--- src/surmeier_lab_to_nwb/zhai2025/interfaces/__init__.py
+++ src/surmeier_lab_to_nwb/zhai2025/interfaces/__init__.py
@@ -8,9 +8,11 @@ from .intracellular_interfaces import (
 )
 from .ophys_interfaces import PrairieViewLineScanInterface
 from .optogenetics_interfaces import PrairieViewOptogeneticsInterface
+from .prairie_view_fluorescence_interface import PrairieViewFluorescenceInterface
 from .trials_interface import DendriticTrialsInterface
 
 __all__ = [
+    "PrairieViewFluorescenceInterface",
     "PrairieViewCurrentClampInterface",
     "PrairieViewVoltageClampInterface",
     "PROTOCOL_STEP_TO_CURRENT",
